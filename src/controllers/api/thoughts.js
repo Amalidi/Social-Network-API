0e393261c0db008e69d8333df91506aa5a68a14d
Amalidi <A.idi12@outlook.com>
2022-08-11 20:43:57 +0100

    updateThought and deleteThought

--- src/controllers/api/thoughts.js
+++ src/controllers/api/thoughts.js
@@ -25,3 +25,63 @@ const getThoughtById = async (req, res) => {
     console.log(`[Error]: Failed to get thought | ${error.message}`);
   }
 };
+
+const createThought = async (req, res) => {
+  try {
+    const { thoughtText, userName } = req.body;
+
+    if (thoughtText && userName) {
+      await Thoughts.create({ userName, thoughtText });
+      return res.json({ success: true });
+    } else {
+      return res.status(400).json({
+        success: false,
+        error: `Please provide a valid thought and username`,
+      });
+    }
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create new thought | ${error.message}`);
+    return res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+const updateThought = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { thoughtText, userName } = req.body;
+
+    if (userName || thoughtText) {
+      await Thoughts.findByIdAndUpdate(id, {
+        userName,
+        thoughtText,
+      });
+
+      return res.json({ success: true });
+    } else res.status(500).json({ success: false });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to update thought | ${error.message}`);
+    return res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+const deleteThought = async (req, res) => {
+  try {
+    const { thoughtId } = req.params;
+
+    // delete the thought
+    await Thoughts.findByIdAndDelete(thoughtId);
+
+    return res.json({ success: true });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to delete thought | ${error.message}`);
+    return res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+module.exports = {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+};
