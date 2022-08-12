409195c9cd23c6c309b8ca9d34525ded5ab71d23
Amalidi <A.idi12@outlook.com>
2022-08-12 20:44:44 +0100

    dotenv

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@
 
 require("dotenv").config();
 
-const express = require("express");
-const mongoose = require("mongoose");
-
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 4000;
